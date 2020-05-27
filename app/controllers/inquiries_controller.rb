53783eab6a2111e9fd8985cddaaef6d574a6dc24
manish kumar <manikuma0405@gmail.com>
2020-05-27 12:09:57 +0900

    お問合せがうまくいかない

--- app/controllers/inquiries_controller.rb
+++ app/controllers/inquiries_controller.rb
@@ -2,11 +2,21 @@ class InquiriesController < ApplicationController
 
 
 def new
-	 @inquirie = Inquirie.new
+	#@support = Support.find_by(params[:support_id])
+	#inquirie = @support.id
+	@inquirie = Inquirie.new
+	#@inquirie = support.inquirie.new(@support_id)
+	 #@inquirie = Inquirie.new(params[:support_id])
 end
 
 def create
-      @inquirie  = Inquirie.create(inquirie_params)
+	@support = Support.find_by(params[:support_id])
+	support = @support.id
+	#@inquirie = @support.inquiries.build([inquirie_params])
+	#@inquirie = Inquirie.create([inquirie_params])
+	#@inquirie  = Inquirie.create(inquirie_params)
+	#@support = Support.find_by(id: params[:support_id])
+    @inquirie  = support.inquirie.create(inquirie_params)
       #@inquirie =  Support.find_by(id: support[:support_id])
    if @inquirie.save
     redirect_to supports_path, notice: "お問い合わせ"
@@ -17,12 +27,11 @@ end
 
 
 
-
 private
 
 def inquirie_params
 
-  params.require(:inquirie).permit(:name, :content, :phone_number, :email)
+  params.require(:inquirie).permit(:name, :content, :phone_number, :email, :support_id)
 
 end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
 resources :sessions, only: [:new, :create, :destroy]
-resources :supports
-resources :inquiries
 
+resources :supports 
+
+resources :inquiries, only:  [:new, :create]
+
+#resources :supports do
+  #resources :inquiries
+#
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
